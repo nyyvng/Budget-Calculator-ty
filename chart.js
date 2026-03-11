f83f824a05ace4047101a38b39c690e71ac9b92d
Azurinite <ligmatitusB@gmail.com>
2026-03-11 14:58:11 -0700

    Made pop-up for <10% savings and decent media query work

--- chart.js
+++ chart.js
@@ -4,6 +4,8 @@ const careerList = [fetch(`https://eecu-data-server.vercel.app/data`)];
 const [...sections] = document.querySelectorAll("section");
 const all_inputs = sections.map((section) => section.querySelectorAll("input"));
 
+const wiseUpTip = document.querySelector('#popUp')
+
 /**
  * @param {NodeListOf<HTMLInputElement>} inputs
  */
@@ -15,6 +17,9 @@ const canvas = document.querySelector("canvas");
 let current_chart = null;
 
 function update() {
+    const inputData = all_inputs.map(inputs => sum(inputs))
+    console.log(inputData)
+
     current_chart?.destroy();
     current_chart = new Chart(canvas, {
         type: 'doughnut',
@@ -23,7 +28,7 @@ function update() {
             datasets: [
                 {
                     label: 'Monthly (USD)',
-                    data: all_inputs.map(inputs => sum(inputs))
+                    data: inputData
                 }
             ]
         },
@@ -41,6 +46,25 @@ function update() {
             }
         }
     });
+
+
+    // Get the total expense amount
+    const amtTotal = inputData.reduce((a, b) => a + b, 0);
+    if (!Number.isNaN(amtTotal)) {
+        // Get savings amount
+        const savingsAmt = inputData[4]
+        // See if savings is less than 10% of total
+        const lessThan10Percent = (savingsAmt/amtTotal) < 0.1
+
+        if ( lessThan10Percent ) {
+            wiseUpTip.classList.remove('hidden')
+        } else {
+            wiseUpTip.classList.add('hidden') 
+        }
+    } else {
+        // Done if an input field is unfilled
+        wiseUpTip.classList.add('hidden')
+    }
 }
 
 document.body.addEventListener('input', () => {
